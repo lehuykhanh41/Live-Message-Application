c4ef703405723dea5ae607288b004009e10581ef
Khanh Le <lehuykhanh41@gmail.com>
2024-07-16 11:09:00 +0700

    Establish an uniconnection

--- back_end/server.js
+++ back_end/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import AuthRouters from './routes/auth.js';
 import MessageRouters from './routes/message.js';
 import UserRouters from './routes/user.js';
@@ -7,7 +8,7 @@ import mongoConnect from './db/MongoConnect.js';
 import cookieParser from 'cookie-parser';
 import {app, server} from './socket/socket.js'
 
-
+const __dirname = path.resolve();
 
 dotenv.config();
 const PORT = process.env.PORT || 7000;
@@ -18,6 +19,11 @@ app.use(cookieParser());
 app.use('/api/auth', AuthRouters);
 app.use('/api/message', MessageRouters);
 app.use('/api/users', UserRouters);
+
+app.use(express.static(path.join(__dirname, '/front_end/dist')));
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "front_end", "dist", "index.html"));
+})
 
 /*
 app.get('/', (req, res)=>{
